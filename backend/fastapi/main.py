72e88035edb67b255b46de0ddee9b69e4f01408c
Kenric Tee <kenricqq@gmail.com>
2025-04-26 03:04:32 -0700

    fastapi basic

---
+++ backend/fastapi/main.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
